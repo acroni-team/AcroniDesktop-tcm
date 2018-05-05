c0f9d7302cac82f6402620cd5df02b87c20df8de
Filipe <lipefigueira11@hotmail.com>
2018-05-04 21:19:49 -0300

    primeira versao do login

---
+++ login-v1.0/FrmLogin.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace login_v1._0
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFund = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFund
+            // 
+            this.lblFund.AutoSize = true;
+            this.lblFund.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.lblFund.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFund.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblFund.Location = new System.Drawing.Point(0, -3);
+            this.lblFund.Name = "lblFund";
+            this.lblFund.Padding = new System.Windows.Forms.Padding(0, 0, 360, 0);
+            this.lblFund.Size = new System.Drawing.Size(360, 39);
+            this.lblFund.TabIndex = 0;
+            this.lblFund.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblFund.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblFund_MouseDown);
+            this.lblFund.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblFund_MouseMove);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Corbel", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.Location = new System.Drawing.Point(27, 59);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(101, 39);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Entrar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.label3.Location = new System.Drawing.Point(26, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 43);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Entrar";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.txt1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.panel1.Location = new System.Drawing.Point(34, 159);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 46);
+            this.panel1.TabIndex = 6;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.Gainsboro;
+            this.txt1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.Gray;
+            this.txt1.Location = new System.Drawing.Point(14, 12);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(265, 22);
+            this.txt1.TabIndex = 7;
+            this.txt1.Enter += new System.EventHandler(this.txt1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.label1.Location = new System.Drawing.Point(31, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.label2.Location = new System.Drawing.Point(31, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Senha";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.txt2);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.panel2.Location = new System.Drawing.Point(34, 246);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(292, 46);
+            this.panel2.TabIndex = 8;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.Gainsboro;
+            this.txt2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.ForeColor = System.Drawing.Color.Gray;
+            this.txt2.Location = new System.Drawing.Point(14, 12);
+            this.txt2.Name = "txt2";
+            this.txt2.PasswordChar = '•';
+            this.txt2.Size = new System.Drawing.Size(265, 22);
+            this.txt2.TabIndex = 7;
+            this.txt2.Enter += new System.EventHandler(this.txt2_Enter);
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.btnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEntrar.FlatAppearance.BorderSize = 0;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Open Sans Extrabold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnEntrar.Location = new System.Drawing.Point(34, 322);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(137, 50);
+            this.btnEntrar.TabIndex = 10;
+            this.btnEntrar.Text = "Enviar";
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Open Sans Extrabold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(181)))));
+            this.btnSair.Location = new System.Drawing.Point(189, 322);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(137, 50);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(360, 420);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblFund);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acroni";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFund;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ login-v1.0/FrmLogin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Text;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace login_v1._0
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+        Point lastPoint;
+        private void lblFund_MouseDown(object sender, MouseEventArgs e)
+        {
+           lastPoint = new Point(e.X, e.Y);
+        }
+        private void lblFund_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) { this.Left += e.X - lastPoint.X; this.Top += e.Y - lastPoint.Y; }
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            txt1.Focus();
+            txt1.ForeColor = Color.Gray;
+
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            txt2.Focus();
+            txt2.ForeColor = Color.Gray;
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if(txt1.Text == "Filipe" && txt2.Text == "filipe")
+            {
+                Application.Exit();
+            } else{
+                txt2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
+                txt1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
+            }
+        }
+
+        private void txt1_Enter(object sender, EventArgs e)
+        {
+            if (txt1.ForeColor == System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(76))))))
+            {
+                txt1.Text = "";
+            }
+            txt1.ForeColor = Color.Gray;
+        }
+
+        private void txt2_Enter(object sender, EventArgs e)
+        {
+            if (txt2.ForeColor == System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(76))))))
+            {
+                txt2.Text = "";
+            }
+            txt2.ForeColor = Color.Gray;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
