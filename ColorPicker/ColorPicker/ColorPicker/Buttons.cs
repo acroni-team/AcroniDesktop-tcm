0bf2ce9ac8fa65d2c079c49abd74e99766ea3e9c
Gravata-Prata <guimotapereira25@gmail.com>
2018-06-28 21:39:38 -0300

    Adicionado projeto ColorPicker

---
+++ ColorPicker/ColorPicker/ColorPicker/Buttons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+class btnColor : Button
+    {
+    /// <summary>
+    /// <para>Metodo que muda a cor de um botão.</para>
+    /// </summary>
+    /// <param name="setColor"></param>
+
+    public void setColor(Color c)
+        {
+        if (this.BackColor == c)
+            this.BackColor = Color.Transparent;
+        else
+            this.BackColor = c;
+        }
+    }
---
+++ ColorPicker/ColorPicker/ColorPicker/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Color Colorpicker { get; set; }
+        List<string> IE = new List<string>();
+
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap b = new Bitmap(pictureBox2.Image);
+            Color c = b.GetPixel(e.X, e.Y);
+            pictureBox3.BackColor = c;
+            Colorpicker = c;
+            lblCor.Text = Colorpicker.ToString();
+        }
+
+        private void btnAbstractus_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.BackColor == Colorpicker)
+                b.BackColor = Color.Transparent;
+            else
+                b.BackColor = Colorpicker;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox3.BackColor = Colorpicker;
+        }
+
+        private void lblCorTotal_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is btnColor)
+                {
+                    (c as Button).BackColor = Colorpicker;
+                }
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter outfile = new StreamWriter(@"C:\Users\Public\teste.acr"))
+            {
+                IE.Add("fez?");
+                IE.Add("n fez");
+                for (int i = 0; i < IE.Count; i++)
+                {
+                    outfile.WriteLine(IE[i]);
+                }
+                outfile.Flush();
+            }
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            using (StreamReader outfile = new StreamReader(@"C:\Users\Public\teste.acr"))
+            {
+                label1.Text = outfile.ReadToEnd();
+                outfile.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (InstalledFontCollection col = new InstalledFontCollection())
+            {
+                foreach (FontFamily fa in col.Families)
+                {
+                    cmbFont.Items.Add(fa.Name);
+                }
+            }
+        }
+
+        private void cmbFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is btnColor)
+                {
+                    c.Font = new System.Drawing.Font(cmbFont.Text, 6.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            foreach (var c in cmbFont.Items)
+            {
+                if (!(Convert.ToString(c).Contains(textBox1.Text)))
+                {
+                    cmbFont.Items.Remove(c);
+                }
+               
+            }
+        }
+    }
+}
