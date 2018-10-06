6f532c811315066f5524cfc2f357599560df93e3
FelipiSantos <gmininsk@gmail.com>
2018-10-06 17:24:05 -0300

    Layout da customização pronto. O resto é capricho.

--- AcroniUI/CustomizingForms/Compacto.cs
+++ AcroniUI/CustomizingForms/Compacto.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 using System.Threading.Tasks;
 using Transitions;
 using AcroniControls;
-using AcroniControls.CustomizingModules;
+using AcroniUI.CustomizingForms.CustomizingModules;
 using System.Collections.Generic;
 using AcroniLibrary.CustomizingMethods.TextFonts;
 using AcroniLibrary;
@@ -16,7 +16,7 @@ namespace AcroniUI.CustomizingForms
 {
     public partial class Compacto : Template
     {
-        // Definição da kbtn genérica
+        // Definição do botão de teclado genérico (kbtn)
         Kbtn keybutton = new Kbtn();
 
         // Definição das propriedades de salvamento
@@ -34,14 +34,16 @@ namespace AcroniUI.CustomizingForms
         // Definição das propriedades do colorpicker 
         private Color Color { get; set; }
 
+        //Ao clicar num botão do teclado
         private void kbtn_Click(object sender, EventArgs e)
         {
             keybutton = (Kbtn)sender;
             keybutton.BackColor = keybutton.SetColor(Color);
-            if(HasChosenAIcon)
+            if (HasChosenAIcon)
                 keybutton.Image = SelectedIcon;
         }
 
+        //Ao clicar no botão de fechar
         private void btnVoltar_Click(object sender, EventArgs e)
         {
             SelectKeyboard __selectKeyboard = new SelectKeyboard();
@@ -54,136 +56,23 @@ namespace AcroniUI.CustomizingForms
         {
             InitializeComponent();
 
-            pnlCorEscolhida.Size = new Size(0, 0);
             //Foreach para arredondar cores do colorpicker
             foreach (Control c_panel in pnlCorEscolhida.Controls)
             {
                 if (c_panel is Panel)
                 {
-                    if (!c_panel.Name.Contains("Divi"))
-                    {
-                        Bunifu.Framework.UI.BunifuElipse elipse = new Bunifu.Framework.UI.BunifuElipse();
-                        elipse.ApplyElipse(c_panel, 5);
-                    }
+                    Bunifu.Framework.UI.BunifuElipse elipse = new Bunifu.Framework.UI.BunifuElipse();
+                    elipse.ApplyElipse(c_panel, 5);
                 }
             }
-
-            pnlHeadColorpicker.Size = new Size(90, 100);
 
             if (Compartilha.editKeyboard)
                 carregaTeclado();
         }
 
-        private void SetKeycapText(object sender, EventArgs e)
-        {
-            //KeycapTextModule keycapTextModule = new KeycapTextModule();
-            //Opacity = 0.1;
-            //keycapTextModule.ShowDialog();
-            //Kbtn kbtn = (Kbtn)sender;
-            //kbtn.Text = keycapTextModule.Maintext;
-        }
-
         #region Métodos do Color Picker
 
         private bool[] __disponibilidade_pnlHistorico { get; set; } = { true, false, false };
-
-        // Esse campo determina se deve-se exibir ou esconder o colorpicker através da lógica de verificar se é um número ímpar ou par
-        private int __checkIfHideOrShow { get; set; } = 0;
-
-        private void pnlColor_Click(object sender, EventArgs e)
-        {
-            if (__checkIfHideOrShow % 2 == 0)
-            {
-                ShowColorpickerHeader();
-                ShowHorizontallyColorpickerBody();
-                ShowVerticallyColorpickerBody();
-            }
-
-            else
-            {
-                HideColorpickerHeader();
-                HideHorizontallyColorpickerBody();
-                HideVerticallyColorpickerBody();
-            }
-
-            __checkIfHideOrShow++;
-        }
-
-        //Mostra o header
-        private async void ShowColorpickerHeader()
-        {
-            for (int x = 90; x < 313; x += 20)
-            {
-                await Task.Delay(1);
-                pnlHeadColorpicker.Width = x;
-            }
-            pnlHeadColorpicker.Width = 313;
-            pnlHeadColorpicker.BackColor = Color.FromArgb(43, 48, 54);
-        }
-
-        //Mostra verticalmente o body do colorpicker
-        private async void ShowVerticallyColorpickerBody()
-        {
-            int y = 0;
-            while (y < 267)
-            {
-                await Task.Delay(1);
-                pnlCorEscolhida.Height = y;
-                y += 20;
-            }
-            pnlCorEscolhida.Height = 267;
-        }
-
-        //Mostra horizontalmente o body do colorpicker
-        private async void ShowHorizontallyColorpickerBody()
-        {
-            int x = 0;
-            while (x < 313)
-            {
-                await Task.Delay(1);
-                pnlCorEscolhida.Width = x;
-                x += 20;
-            }
-            pnlCorEscolhida.Width = 313;
-        }
-
-        //Esconde o header
-        private async void HideColorpickerHeader()
-        {
-            for (int x = 313; x > 90; x -= 20)
-            {
-                await Task.Delay(1);
-                pnlHeadColorpicker.Width = x;
-            }
-            pnlHeadColorpicker.Width = 90;
-            pnlHeadColorpicker.BackColor = Color.Transparent;
-        }
-
-        //Esconde verticalmente o body do colorpicker
-        private async void HideVerticallyColorpickerBody()
-        {
-            int y = 267;
-            while (y > 0)
-            {
-                await Task.Delay(1);
-                pnlCorEscolhida.Height = y;
-                y -= 20;
-            }
-            pnlCorEscolhida.Height = 0;
-        }
-
-        //Esconde verticalmente o body do colorpicker
-        private async void HideHorizontallyColorpickerBody()
-        {
-            int x = 313;
-            while (x > 0)
-            {
-                await Task.Delay(1);
-                pnlCorEscolhida.Width = x;
-                x -= 20;
-            }
-            pnlCorEscolhida.Width = 0;
-        }
 
         //Clicks que ocorrem ao selecionar uma cor
         private void escolhe_cor(object sender, EventArgs e)
@@ -197,10 +86,8 @@ namespace AcroniUI.CustomizingForms
             t_cor.add(pnlColor, "BackColor", p.BackColor);
             t_cor.run();
 
-            //desaparece_colorpicker();
-            __checkIfHideOrShow = 0;
-
             Color = p.BackColor;
+            //Será usado no kbtn_click
 
             if (__disponibilidade_pnlHistorico[0])
             {
@@ -222,33 +109,23 @@ namespace AcroniUI.CustomizingForms
             }
         }
 
-        private Color previousColor;
-        int checkIfItsFirstTime;
-
-        private async void pnlColor_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (checkIfItsFirstTime == 0)
-                previousColor = pnlColor.BackColor;
-            checkIfItsFirstTime++;
-            pnlColor.BackColor = Color.FromArgb(20, pnlColor.BackColor);
-            foreach (Control c in this.Controls)
-            {
-                if (!c.Name.Contains("pnlCo") || !c.Name.Equals("pnlHeadColorpicker"))
-                    c.Visible = false;
-            }
-            lblEscolherCores.Visible = true;
-
-        }
-
-        private void pnlColor_MouseLeave(object sender, EventArgs e)
-        {
-            pnlColor.BackColor = previousColor;
-            lblEscolherCores.Visible = false;
-        }
+        #region Hover para cada uma das cores do colorpicker
 
         #endregion
 
-        #region Fontes das teclas
+        #endregion
+
+        #region Fontes das teclas e texto
+
+        private void SetKeycapText(object sender, EventArgs e)
+        {
+            //KeycapTextModule keycapTextModule = new KeycapTextModule();
+            //Opacity = 0.1;
+            //keycapTextModule.ShowDialog();
+            //Kbtn kbtn = (Kbtn)sender;
+            //kbtn.Text = keycapTextModule.Maintext;
+        }
+
 
         #region Definição dos métodos de alinhamento
 
@@ -486,12 +363,12 @@ namespace AcroniUI.CustomizingForms
                     insertableArray.Add(ImageQueue.ToArray()[aux]);
                 }
             }
-            for (int i = 0; i < ImageQueue.Count; i++)
-                (pnlIcons.Controls[$"picBoxIcon{i + 1}"] as PictureBox).Image = insertableArray[i];
+            //for (int i = 0; i < ImageQueue.Count; i++)
+                //(pnlIcons.Controls[$"picBoxIcon{i + 1}"] as PictureBox).Image = insertableArray[i];
         }
 
         private void picIcons_Click(object sender, EventArgs e)
-        {
+        { 
             if (sender != null)
             {
                 PictureBox __icon = (PictureBox)sender;
@@ -499,6 +376,13 @@ namespace AcroniUI.CustomizingForms
                 HasChosenAIcon = true;
             }
         }
+
         #endregion
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Button btnColor = (Button)sender;
+            btnColor.Size = new Size(btnColor.Width + 25, btnColor.Height + 25);
+        }
     }
 }
---
+++ AcroniUI/CustomizingForms/CustomizingModules/KeycapIconModule.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace AcroniUI.CustomizingForms.CustomizingModules
+{
+    partial class KeycapIconModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlIcons = new System.Windows.Forms.Panel();
+            this.picBoxIcon10 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon9 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon8 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon7 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon6 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon5 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon4 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon3 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon2 = new System.Windows.Forms.PictureBox();
+            this.picBoxIcon1 = new System.Windows.Forms.PictureBox();
+            this.pnlIcons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlIcons
+            // 
+            this.pnlIcons.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.pnlIcons.Controls.Add(this.picBoxIcon10);
+            this.pnlIcons.Controls.Add(this.picBoxIcon9);
+            this.pnlIcons.Controls.Add(this.picBoxIcon8);
+            this.pnlIcons.Controls.Add(this.picBoxIcon7);
+            this.pnlIcons.Controls.Add(this.picBoxIcon6);
+            this.pnlIcons.Controls.Add(this.picBoxIcon5);
+            this.pnlIcons.Controls.Add(this.picBoxIcon4);
+            this.pnlIcons.Controls.Add(this.picBoxIcon3);
+            this.pnlIcons.Controls.Add(this.picBoxIcon2);
+            this.pnlIcons.Controls.Add(this.picBoxIcon1);
+            this.pnlIcons.Location = new System.Drawing.Point(260, 163);
+            this.pnlIcons.Name = "pnlIcons";
+            this.pnlIcons.Size = new System.Drawing.Size(281, 125);
+            this.pnlIcons.TabIndex = 5;
+            // 
+            // picBoxIcon10
+            // 
+            this.picBoxIcon10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon10.Location = new System.Drawing.Point(219, 64);
+            this.picBoxIcon10.Name = "picBoxIcon10";
+            this.picBoxIcon10.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon10.TabIndex = 10;
+            this.picBoxIcon10.TabStop = false;
+            // 
+            // picBoxIcon9
+            // 
+            this.picBoxIcon9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon9.Location = new System.Drawing.Point(168, 64);
+            this.picBoxIcon9.Name = "picBoxIcon9";
+            this.picBoxIcon9.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon9.TabIndex = 9;
+            this.picBoxIcon9.TabStop = false;
+            // 
+            // picBoxIcon8
+            // 
+            this.picBoxIcon8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon8.Location = new System.Drawing.Point(117, 64);
+            this.picBoxIcon8.Name = "picBoxIcon8";
+            this.picBoxIcon8.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon8.TabIndex = 6;
+            this.picBoxIcon8.TabStop = false;
+            // 
+            // picBoxIcon7
+            // 
+            this.picBoxIcon7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon7.Location = new System.Drawing.Point(66, 64);
+            this.picBoxIcon7.Name = "picBoxIcon7";
+            this.picBoxIcon7.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon7.TabIndex = 7;
+            this.picBoxIcon7.TabStop = false;
+            // 
+            // picBoxIcon6
+            // 
+            this.picBoxIcon6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon6.Location = new System.Drawing.Point(15, 64);
+            this.picBoxIcon6.Name = "picBoxIcon6";
+            this.picBoxIcon6.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon6.TabIndex = 8;
+            this.picBoxIcon6.TabStop = false;
+            // 
+            // picBoxIcon5
+            // 
+            this.picBoxIcon5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon5.Location = new System.Drawing.Point(219, 13);
+            this.picBoxIcon5.Name = "picBoxIcon5";
+            this.picBoxIcon5.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon5.TabIndex = 5;
+            this.picBoxIcon5.TabStop = false;
+            // 
+            // picBoxIcon4
+            // 
+            this.picBoxIcon4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon4.Location = new System.Drawing.Point(168, 13);
+            this.picBoxIcon4.Name = "picBoxIcon4";
+            this.picBoxIcon4.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon4.TabIndex = 3;
+            this.picBoxIcon4.TabStop = false;
+            // 
+            // picBoxIcon3
+            // 
+            this.picBoxIcon3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon3.Location = new System.Drawing.Point(117, 13);
+            this.picBoxIcon3.Name = "picBoxIcon3";
+            this.picBoxIcon3.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon3.TabIndex = 0;
+            this.picBoxIcon3.TabStop = false;
+            // 
+            // picBoxIcon2
+            // 
+            this.picBoxIcon2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon2.Location = new System.Drawing.Point(66, 13);
+            this.picBoxIcon2.Name = "picBoxIcon2";
+            this.picBoxIcon2.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon2.TabIndex = 1;
+            this.picBoxIcon2.TabStop = false;
+            // 
+            // picBoxIcon1
+            // 
+            this.picBoxIcon1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxIcon1.Location = new System.Drawing.Point(15, 13);
+            this.picBoxIcon1.Name = "picBoxIcon1";
+            this.picBoxIcon1.Size = new System.Drawing.Size(40, 40);
+            this.picBoxIcon1.TabIndex = 2;
+            this.picBoxIcon1.TabStop = false;
+            // 
+            // KeycapIconModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlIcons);
+            this.Name = "KeycapIconModule";
+            this.Opacity = 1D;
+            this.Text = "KeycapIconModule";
+            this.pnlIcons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIcon1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlIcons;
+        private System.Windows.Forms.PictureBox picBoxIcon10;
+        private System.Windows.Forms.PictureBox picBoxIcon9;
+        private System.Windows.Forms.PictureBox picBoxIcon8;
+        private System.Windows.Forms.PictureBox picBoxIcon7;
+        private System.Windows.Forms.PictureBox picBoxIcon6;
+        private System.Windows.Forms.PictureBox picBoxIcon5;
+        private System.Windows.Forms.PictureBox picBoxIcon4;
+        private System.Windows.Forms.PictureBox picBoxIcon3;
+        private System.Windows.Forms.PictureBox picBoxIcon2;
+        private System.Windows.Forms.PictureBox picBoxIcon1;
+    }
+}
