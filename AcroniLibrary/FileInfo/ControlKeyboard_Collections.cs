6d045edcf410a2b4f8b72ea564603328bf3d45e4
FelipiSantos <gmininsk@gmail.com>
2018-10-11 17:53:23 -0300

    Solução resetada.

--- AcroniLibrary/FileInfo/ControlKeyboard_Collections.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace AcroniLibrary.FileInfo
-{
-    public class ControlKeyboard_Collections
-    {
-        public static List<ControlKeyboard> keyboardsControl = new List<ControlKeyboard>();
-    }
-}
