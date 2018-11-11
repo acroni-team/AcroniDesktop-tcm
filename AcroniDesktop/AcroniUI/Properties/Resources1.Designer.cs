6a638fe6a867198ae19d7cf7e9871d5284e4d259
FelipiSantos <gmininsk@gmail.com>
2018-11-11 19:29:13 -0200

    Imagem da minha conta func

--- AcroniDesktop/AcroniUI/Properties/Resources1.Designer.cs
+++ AcroniDesktop/AcroniUI/Properties/Resources1.Designer.cs
@@ -113,6 +113,16 @@ namespace AcroniUI.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap padrao {
+            get {
+                object obj = ResourceManager.GetObject("padrao", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap shiftarrow {
             get {
                 object obj = ResourceManager.GetObject("shiftarrow", resourceCulture);
