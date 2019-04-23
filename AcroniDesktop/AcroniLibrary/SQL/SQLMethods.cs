0a86a5e2bda780d694d3a1ed1254fb3123356d9b
Gravata_Prata <guimotapereira25@gmail.com>
2019-04-23 20:42:29 -0300

    Correções na conexão com o banco
    (Talvez) Corrigido o erro de conexão com o banco

--- AcroniDesktop/AcroniLibrary/SQL/SQLMethods.cs
+++ AcroniDesktop/AcroniLibrary/SQL/SQLMethods.cs
@@ -8,151 +8,136 @@ namespace AcroniLibrary.SQL
     {
         public static bool SELECT_HASROWS(String commandSELECT)
         {
+            bool HR = false;
+            SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao);
             try
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao))
-                {
-                    newConnection.Open();
-                    using (SqlCommand select = new SqlCommand(commandSELECT, newConnection))
-                    {
-                        using (SqlDataReader hasName = select.ExecuteReader())
-                        {
-                            return hasName.HasRows;
-                        }
-                    }
-                }
+                newConnection.Open();
             }
             catch (Exception)
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS","")))
+                newConnection.Close();
+                newConnection.Dispose();
+                newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS", ""));
+                newConnection.Open();
+            }finally
+            {
+                using (SqlCommand select = new SqlCommand(commandSELECT, newConnection))
                 {
-                    newConnection.Open();
-                    using (SqlCommand select = new SqlCommand(commandSELECT, newConnection))
+                    using (SqlDataReader hasRows = select.ExecuteReader())
                     {
-                        using (SqlDataReader hasName = select.ExecuteReader())
-                        {
-                            return hasName.HasRows;
-                        }
+                        HR = hasRows.HasRows;
                     }
                 }
+                newConnection.Close();
+                newConnection.Dispose();
             }
+            return HR;
         }
         
         public static List<object> SELECT(string SelectCommand)
         {
             List<object> ret = new List<object> { };
-            try {
-                using (SqlConnection newConn = new SqlConnection(SQLConnection.nome_conexao))
-                {
-                    newConn.Open();
-                    using (SqlCommand newComm = new SqlCommand((SelectCommand.Contains("SELECT") || SelectCommand.Contains("select")) ? SelectCommand : "SELECT " + SelectCommand, newConn))
-                    {
-                        using (SqlDataReader reader = newComm.ExecuteReader())
-                        {
-                            reader.Read();
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                ret.Add(reader[i]);
-                            }
-                        }
-                    }
-                }
-            } catch (Exception)
+            SqlConnection newConn = new SqlConnection(SQLConnection.nome_conexao);
+            try
             {
-                using (SqlConnection newConn = new SqlConnection(SQLConnection.nome_conexao.Replace("//SQLEXPRESS","")))
+                newConn.Open();
+            }
+            catch (Exception)
+            {
+                newConn.Close();
+                newConn.Dispose();
+                newConn = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS", ""));
+                newConn.Open();
+            } finally
+            {
+                using (SqlCommand newComm = new SqlCommand((SelectCommand.Contains("SELECT") || SelectCommand.Contains("select")) ? SelectCommand : "SELECT " + SelectCommand, newConn))
                 {
-                    newConn.Open();
-                    using (SqlCommand newComm = new SqlCommand((SelectCommand.Contains("SELECT") || SelectCommand.Contains("select")) ? SelectCommand : "SELECT " + SelectCommand, newConn))
+                    using (SqlDataReader reader = newComm.ExecuteReader())
                     {
-                        using (SqlDataReader reader = newComm.ExecuteReader())
+                        reader.Read();
+                        for (int i = 0; i < reader.FieldCount; i++)
                         {
-                            reader.Read();
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                ret.Add(reader[i]);
-                            }
+                            ret.Add(reader[i]);
                         }
                     }
                 }
+                newConn.Close();
+                newConn.Dispose();
             }
             return ret;
         }
 
         public static int INSERT_INTO(String commandINSERT)
         {
+            int rowsAffected = 0;
+            SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao);
             try
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao))
-                {
-                    newConnection.Open();
-                    using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
-                    {
-                        return insert.ExecuteNonQuery();
-                    }
-                }
+                newConnection.Open();               
             } catch (Exception)
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("//SQLEXPRESS","")))
+                newConnection.Close();
+                newConnection.Dispose();
+                newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS", ""));
+                newConnection.Open();
+            }finally
+            {
+                using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
                 {
-                    newConnection.Open();
-                    using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
-                    {
-                        return insert.ExecuteNonQuery();
-                    }
+                    rowsAffected = insert.ExecuteNonQuery();
                 }
+                newConnection.Close();
+                newConnection.Dispose();
             }
+            return rowsAffected;
         }
         
         public static int UPDATE(String commandUPDATE)
         {
+            int rowsAffected = 0;
+            SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao);
             try
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao))
-                {
-                    newConnection.Open();
-                    using (SqlCommand update = new SqlCommand(commandUPDATE, newConnection))
-                    {
-                        return update.ExecuteNonQuery();
-                    }
-                }
+                newConnection.Open();
             } catch(Exception)
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("//SQLEXPRESS","")))
+                newConnection.Close();
+                newConnection.Dispose();
+                newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS", ""));
+                newConnection.Open();
+            }finally
+            {
+                using (SqlCommand update = new SqlCommand(commandUPDATE, newConnection))
                 {
-                    newConnection.Open();
-                    using (SqlCommand update = new SqlCommand(commandUPDATE, newConnection))
-                    {
-                        return update.ExecuteNonQuery();
-                    }
+                    rowsAffected = update.ExecuteNonQuery();
                 }
             }
+            return rowsAffected;
         }
 
         public static int INSERT_INTO(String commandINSERT, byte[] parameter)
         {
+            int rowsAffected = 0;
+            SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao);
             try
-            {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao))
-                {
-                    newConnection.Open();
-                    using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
-                    {
-                        insert.Parameters.AddWithValue("@image", parameter);
-                        return insert.ExecuteNonQuery();
-                    }
-                }
+            {   
+                newConnection.Open();
             } catch (Exception)
             {
-                using (SqlConnection newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("//SQLEXPRESS","")))
+                newConnection.Close();
+                newConnection.Dispose();
+                newConnection = new SqlConnection(SQLConnection.nome_conexao.Replace("\\SQLEXPRESS", ""));
+                newConnection.Open();
+            }finally
+            {
+                using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
                 {
-                    newConnection.Open();
-                    using (SqlCommand insert = new SqlCommand(commandINSERT, newConnection))
-                    {
-                        insert.Parameters.AddWithValue("@image", parameter);
-                        return insert.ExecuteNonQuery();
-                    }
+                    insert.Parameters.AddWithValue("@image", parameter);
+                    rowsAffected = insert.ExecuteNonQuery();
                 }
             }
+            return rowsAffected;
         }
     }
 }
